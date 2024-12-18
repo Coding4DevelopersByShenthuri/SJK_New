a1af84c92098649e169ca1d8c4e857181d15fa06
Shenthuri Maran <168589614+Coding4DevelopersByShenthuri@users.noreply.github.com>
2024-12-18 21:04:47 +0530

    ok

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,5 +1,4 @@
 import basket_icon from './basket_icon.png'
-import logo from './logo.png'
 import search_icon from './search_icon.png'
 import menu_1 from './menu_1.png'
 import menu_2 from './menu_2.png'
@@ -46,8 +45,6 @@ import food_32 from './food_32.png'
 import add_icon_white from './add_icon_white.png'
 import add_icon_green from './add_icon_green.png'
 import remove_icon_red from './remove_icon_red.png'
-import app_store from './app_store.png'
-import play_store from './play_store.png'
 import linkedin_icon from './linkedin_icon.png'
 import facebook_icon from './facebook_icon.png'
 import twitter_icon from './twitter_icon.png'
@@ -60,15 +57,12 @@ import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 
 export const assets = {
-    logo,
     basket_icon,
     search_icon,
     rating_starts,
     add_icon_green,
     add_icon_white,
     remove_icon_red,
-    app_store,
-    play_store,
     linkedin_icon,
     facebook_icon,
     twitter_icon,
