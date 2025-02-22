fa3b5dd7cda5ac72b557865166e4aa5bc508efaf
Shenthuri Maran <168589614+Coding4DevelopersByShenthuri@users.noreply.github.com>
2025-02-22 12:09:41 +0530

    ok

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -5,7 +5,48 @@ import fs from "fs";
 // add food item
 
 const addFood = async (req, res) => {
+
+    let image_filename =  `${req.file.filename}`;
     
+    const food = new foodModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        Image: image_filename,
+        category: req.body.category
+    })
+    try {
+        await food.save();
+        res.json({success: true, message:'Food item added successfully'})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message:'Failed to add food item'})
+    }
 }
 
-export {addFood}
+// list food items
+const listFood = async (req, res) => {
+    try {
+        const foods = await foodModel.find({});
+        res.json({success: true, data:foods})
+    } catch (error) {
+      res.json({success: false, message:'Failed to list food items'})
+    }
+}
+
+// remove food item
+
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.Image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success: true, message:'Food item removed successfully'})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message:'Failed to remove food item'})
+    }
+}
+
+export {addFood, listFood, removeFood}
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addFood } from "../controllers/foodController.js";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
 
 const foodRouter = express.Router();
@@ -13,7 +13,11 @@ const storage = multer.diskStorage({
     }
 })
 
-foodRouter.post('/add', addFood)
+const upload = multer({storage:storage})
+
+foodRouter.post('/add',upload.single('image'), addFood)
+foodRouter.get('/list', listFood)
+foodRouter.post('/remove', removeFood);
 
 
 
