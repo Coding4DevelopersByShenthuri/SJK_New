5113b915c0c3382186ed148551f25091d465059d
Shenthuri Maran <168589614+Coding4DevelopersByShenthuri@users.noreply.github.com>
2025-02-26 09:19:27 +0530

    ok

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -1,13 +1,14 @@
-import mongoose, { Types } from "mongoose";
+import mongoose from "mongoose";
 
 const foodSchema = new mongoose.Schema({
-    name: {type:String, required:true},
-    description: {type:String, required:true},
-    price: {type:Number, required:true},
-    Image: {type:String, required:true},
-    category: {type:String, required:true}
-})
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    normalPrice: { type: Number, required: true },  // Added normalPrice
+    fullPrice: { type: Number, required: true },    // Added fullPrice
+    image: { type: String, required: true },
+    category: { type: String, required: true }
+});
 
-const foodModel = mongoose.models.food || mongoose.model('food', foodSchema)
+const foodModel = mongoose.models.food || mongoose.model('food', foodSchema);
 
 export default foodModel;
