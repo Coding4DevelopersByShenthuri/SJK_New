d42f193a3fd779a451583d179d8eb7ca0a206031
Shenthuri Maran <168589614+Coding4DevelopersByShenthuri@users.noreply.github.com>
2025-02-27 20:02:00 +0530

    ok

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate, NavLink } from 'react-router-dom';
 import './Sidebar.css';
-import {assets} from '../../assets/assets'
-import { NavLink } from 'react-router-dom';
+import { assets } from '../../assets/assets';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/add'); // Redirect to Add Items page on initial load
+  }, [navigate]);
+
   return (
     <div className='sidebar'>
-        <div className='sidebar-options'>
-          <NavLink to='/add' className='sidebar-option'>
-            <img src={assets.add_icon} alt='' />
-            <p>Add Items</p>
-          </NavLink>
-          <NavLink to='/list' className='sidebar-option'>
-            <img src={assets.order_icon} alt='' />
-            <p>List Items</p>
-          </NavLink>
-          <NavLink to='/orders' className='sidebar-option'>
-            <img src={assets.order_icon} alt='' />
-            <p>Orders</p>
-          </NavLink>
-        </div>
+      <div className='sidebar-options'>
+        <NavLink to='/add' className='sidebar-option'>
+          <img src={assets.add_icon} alt='' />
+          <p>Add Items</p>
+        </NavLink>
+        <NavLink to='/list' className='sidebar-option'>
+          <img src={assets.order_icon} alt='' />
+          <p>List Items</p>
+        </NavLink>
+        <NavLink to='/orders' className='sidebar-option'>
+          <img src={assets.order_icon} alt='' />
+          <p>Orders</p>
+        </NavLink>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar;
